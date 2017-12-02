68cfca9b58d7e98e53958b2785e34da90389f83b
otterdanny <33238284+otterdanny@users.noreply.github.com>
2017-12-02 17:03:00 -0500

    Changing Background

---
+++ SpaceShmup/Assets/__Scripts/ChangeBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBackground : MonoBehaviour {
+
+    public GameObject[] items;
+
+    void Start()
+    {
+        items[0].SetActive(true);
+        items[1].SetActive(true);
+        items[2].SetActive(false);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (EnemiesLeftTxt.enemiesLeft == 90)
+        {
+            items[0].SetActive(false);
+            items[1].SetActive(false);
+            items[2].SetActive(true);
+        }
+    }
+}
