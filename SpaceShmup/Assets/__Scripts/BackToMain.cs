e30505810f0e7712d69dac842a094d2daafdc740
otterdanny <33238284+otterdanny@users.noreply.github.com>
2017-12-04 07:18:30 -0500

    Pause Button

---
+++ SpaceShmup/Assets/__Scripts/BackToMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMain : MonoBehaviour {
+    
+    // Update is called once per frame
+    public void  ReturnMainMenu(int sceneMain) {
+        SceneManager.LoadScene(sceneMain);        
+    }
+}
---
+++ SpaceShmup/Assets/__Scripts/Pause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+
+    public Text pauseTxt;
+    public GameObject menuBtn;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseTxt.text = "PAUSE";
+        pauseTxt.enabled = false;
+        menuBtn.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                pauseTxt.enabled = true;
+                menuBtn.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseTxt.enabled = false;
+                menuBtn.SetActive(false);
+            }
+        }
+    }
+}
