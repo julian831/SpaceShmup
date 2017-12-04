260106c2a23635d1e80c0b0c5ed3ba85796f5d97
otterdanny <33238284+otterdanny@users.noreply.github.com>
2017-12-04 08:31:07 -0500

    Change Scene with Time

---
+++ SpaceShmup/Assets/__Scripts/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Timer : MonoBehaviour
+{
+    public static int secondsPassed;
+    public static int minutesPassed;
+    public Text timerText;
+    public static bool stopTimer;
+
+    //public AudioClip clip;
+    //AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("TimeCounter");
+        minutesPassed = 0;
+        secondsPassed = 0;
+        stopTimer = false;
+        //audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerText.text = ("Time: " + minutesPassed + ":" + secondsPassed.ToString("00"));
+
+        if (stopTimer == true) 
+        { 
+            StopTimer();
+            stopTimer = false;
+        }
+    }
+
+
+    IEnumerator TimeCounter()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            secondsPassed++;
+            if(secondsPassed == 60)
+            {
+                secondsPassed = 0;
+                minutesPassed++;
+            }
+        }
+    }
+
+    public void StopTimer()
+    {
+        StopCoroutine("TimeCounter");
+    }
+
+}
