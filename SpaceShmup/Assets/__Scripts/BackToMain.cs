f5f2a74c4f14c3f7deac2d61f5c5a58ef5861b2b
otterdanny <33238284+otterdanny@users.noreply.github.com>
2017-12-04 17:54:27 -0500

    Removed some Buttons

--- SpaceShmup/Assets/__Scripts/BackToMain.cs
+++ SpaceShmup/Assets/__Scripts/BackToMain.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class BackToMain : MonoBehaviour {
     
-    // Update is called once per frame
     public void  ReturnMainMenu(int sceneMain) {
         SceneManager.LoadScene(sceneMain);        
     }
